ec0b83f599edf1dce1b1af2cbcae48d81cfdb26f
Florent FERRY <florent@ouvrages-web.fr>
2017-04-03 11:11:37 +0200

    Fix need of tinymce-rails gem to be required

--- lib/bootstrap_form_rich_text_field.rb
+++ lib/bootstrap_form_rich_text_field.rb
@@ -1,6 +1,7 @@
 require "bootstrap_form_rich_text_field/version"
 require "bootstrap_form_rich_text_field/rich_text_field"
 require "bootstrap_form/form_builder"
+require "tinymce-rails"
 
 module BootstrapFormRichTextField
   include RichTextField
